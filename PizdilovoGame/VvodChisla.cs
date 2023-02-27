7eb5b9aad00c1fafae4ef15e6ecf93e271fc60fd
Krechin94 <andreikrech@gmail.com>
2023-02-27 13:44:44 +0300

    Loggin

--- PizdilovoGame/VvodChisla.cs
+++ PizdilovoGame/VvodChisla.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.IO;
 
 namespace PizdilovoGame
 {
@@ -15,10 +15,13 @@ namespace PizdilovoGame
                 Chislo = int.Parse(Console.ReadLine());
             }
             catch (Exception exp)
-            {
+            {                
+                StreamWriter sw = new StreamWriter("C:\\Users\\79111\\Desktop\\Git\\Log.txt");
+                sw.WriteLine(exp.Message);
                 Console.WriteLine(exp.Message);
                 if (Chislo != 1 && Chislo != 2 && Chislo != 3)
                     throw new WrongUserInputException();
+                sw.WriteLine("пошел нахуй");
                 return;
             }        
             Number = Chislo;
