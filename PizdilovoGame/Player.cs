355bfcc50a8209c6c90a42d7e251745bd9a613e7
axtox <axtoox@protonmail.com>
2023-03-03 05:56:32 +0300

    Player.cs styling and code cleanup

--- PizdilovoGame/Player.cs
+++ PizdilovoGame/Player.cs
@@ -6,17 +6,15 @@ namespace PizdilovoGame
 {
     public class Player : IPlayer
     {
-        Random random = new Random();
-        public int HP { get; set; }
-
-        public int Stamina { get; set; }
-        public string Name { get; set; }
-        
-        public string Nation { get; set; }
-
-        VvodChisla vvodChisla = new VvodChisla();
-
         private IWeapon _currentWeapon;
+
+        private readonly Random _random = new Random();
+        private readonly VvodChisla _vvodChisla = new VvodChisla();
+
+        public int HP { get; set; }
+        public int Stamina { get; set; }
+        public string Name { get; set; }        
+        public string Nation { get; set; }
 
         public void Udar(IPlayer enemy)
         {                      
@@ -29,44 +27,43 @@ namespace PizdilovoGame
             {
                 enemy.HP = enemy.HP - _currentWeapon.Uron;
                 this.Stamina = this.Stamina - _currentWeapon.Cost;
-                Console.WriteLine($"{Name} нанес {_currentWeapon.Uron} урона. У {enemy.Name} осталось {enemy.HP} хп");                
-                Baffi buf = new Baffi();
+                Console.WriteLine($"{Name} нанес {_currentWeapon.Uron} урона. У {enemy.Name} осталось {enemy.HP} хп");
+                
                 Console.WriteLine("Куда бить 1 - голова, 2 - туловище, 3 - ноги");
-                int kuda= -1;
-                vvodChisla.Vvod();
-                kuda = vvodChisla.Number;
-                    Console.WriteLine(this.Name + " " + this.HP);
-                    switch (kuda)
-                    {
-                        case 1:
-                            {
-                                int a = random.Next(0, 4);
-                                enemy.HP = enemy.HP - a;
-                                Console.WriteLine($" ты наносишь {a} урона");
-                                break;
+                _vvodChisla.Vvod();
 
-                            }
-                        case 2:
+                Console.WriteLine(this.Name + " " + this.HP);
+
+                var kuda = _vvodChisla.Number;
+                switch (kuda)
+                {
+                    case 1:
+                        {
+                            int a = _random.Next(0, 4);
+                            enemy.HP = enemy.HP - a;
+                            Console.WriteLine($" ты наносишь {a} урона");
+                            break;
+                        }
+                    case 2:
+                        {
+                            int b = _random.Next(1, 3);
+                            enemy.HP = enemy.HP - b;
+                            Console.WriteLine($" ты наносишь {b} урона");
+                            break;
+                        }
+                    case 3:
+                        {
+                            enemy.HP = enemy.HP - 1;
+                            Console.WriteLine($" ты наносишь 1 урон");
+                            int d = _random.Next(0, 2);
+                            if (d == 0)
                             {
-                                int b = random.Next(1, 3);
-                                enemy.HP = enemy.HP - b;
-                                Console.WriteLine($" ты наносишь {b} урона");
-                                break;
-                            }
-                        case 3:
-                            {
-                            
-                                enemy.HP = enemy.HP - 1;
-                                Console.WriteLine($" ты наносишь 1 урон");
-                                int d = random.Next(0, 2);
-                                if (d == 0)
-                                {
                                 Console.WriteLine("TEBE POVEZLO");
                                 this.Udar(enemy);                                    
-                                }
-                                break;
-                            }                      
-                    }                                                  
+                            }
+                            break;
+                        }                      
+                }                                                  
             }
         }
 
@@ -74,6 +71,7 @@ namespace PizdilovoGame
         {
             _currentWeapon = weapon;
         }
+
         public override string ToString()
         {
             return $" {Name} Nation: {Nation}  Stamina: {Stamina} HP: {HP} Weapon: {_currentWeapon}";
