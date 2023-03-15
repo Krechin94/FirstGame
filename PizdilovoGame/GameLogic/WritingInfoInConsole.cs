ffaa4711952a55ec41e7fef98ee38e72ecf55edb
Krechin94 <andreikrech@gmail.com>
2023-03-15 19:21:11 +0300

    более правильный и читабельный код в логике вывода в консоль

--- PizdilovoGame/GameLogic/WritingInfoInConsole.cs
+++ PizdilovoGame/GameLogic/WritingInfoInConsole.cs
@@ -1,11 +1,12 @@
 ﻿using PizdilovoGame.Rassi;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PizdilovoGame.GameLogic
 {
     internal class WritingInfoInConsole
     {
-        int longest;
         int raznitsahp;
         int raznitsaName;
         int raznitsaMana;
@@ -19,49 +20,42 @@ namespace PizdilovoGame.GameLogic
             var playerMana1 = $"{player1.Stamina} Mana";
             var playerMana2 = $"{player2.Stamina} Mana";
 
+            var displayInfoLengths = new List<int>
+        {
+            playerName1.Length,
+            playerName2.Length,
+            playerHp1.Length,
+            playerHp2.Length,
+            playerMana1.Length,
+            playerMana2.Length,
+        };
 
-            if (playerName1.Length > playerHp1.Length)
-            {
-                if (playerName1.Length > playerMana1.Length)
-                {
-                    longest = playerName1.Length;
-                    raznitsaName = 0;
-                    raznitsahp = longest - playerHp1.Length;
-                    raznitsaMana = longest - playerMana1.Length;
-                }
-                else
-                {
-                    longest = playerMana1.Length;
-                    raznitsaName = longest - playerName1.Length;
-                    raznitsahp = longest - playerHp1.Length;
-                    raznitsaMana = 0;
-                }
-            }
-            else
-            {
-                if (playerHp1.Length > playerMana1.Length)
-                {
-                    longest = playerHp1.Length;
-                    raznitsaName = longest - playerName1.Length;
-                    raznitsahp = 0;
-                    raznitsaMana = longest - playerMana1.Length;
-                }
-                else
-                {
-                    longest = playerMana1.Length;
-                    raznitsaName = longest - playerName1.Length;
-                    raznitsahp = longest - playerHp1.Length;
-                    raznitsaMana = 0;
-                }
-            }
+            displayInfoLengths.Sort();
+            var longest = displayInfoLengths.Last();
+
+            raznitsaName = longest - playerName1.Length;
+            raznitsahp = longest - playerHp1.Length;
+            raznitsaMana = longest - playerMana1.Length;
+            playerName1 = Viravnivanie(raznitsaName, playerName1);
+            playerHp1 = Viravnivanie(raznitsahp, playerHp1);
+            playerMana1 = Viravnivanie(raznitsaMana, playerMana1);
 
             Console.SetCursorPosition((Console.WindowWidth) - longest * 3, 0);
-            Console.WriteLine($"{playerName1}{arrayProbel[raznitsaName]} |{playerName2}");
+            Console.WriteLine($"{playerName1} |{playerName2}");
             Console.SetCursorPosition((Console.WindowWidth) - longest * 3, 1);
-            Console.WriteLine($"{playerHp1}{arrayProbel[raznitsahp]} |{playerHp2}");
+            Console.WriteLine($"{playerHp1} |{playerHp2}");
             Console.SetCursorPosition((Console.WindowWidth) - longest * 3, 2);
-            Console.WriteLine($"{playerMana1}{arrayProbel[raznitsaMana]} |{playerMana2}");
+            Console.WriteLine($"{playerMana1} |{playerMana2}");
             Console.SetCursorPosition(0, 0);
+        }
+
+        public string Viravnivanie(int raznitsaDlini, string text)
+        {
+            for (int i = 0; i < raznitsaDlini; i++)
+            {
+                text += " ";
+            }
+            return text;
         }
     }
 }
